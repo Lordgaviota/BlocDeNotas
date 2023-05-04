8abfcedc2c3676096be9b5aa2df2430b62f580a6
Lordgaviota <javiercalo02@gmail.com>
2023-05-04 14:51:08 +0200

    Initial commit

---
+++ src/main/java/com/aulanosa/bloc/BlocDeNotas/BlocDeNotasApplication.java
@@ -0,0 +1,13 @@
+package com.aulanosa.bloc.BlocDeNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulanosa/bloc/BlocDeNotas/BlocDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulanosa.bloc.BlocDeNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
